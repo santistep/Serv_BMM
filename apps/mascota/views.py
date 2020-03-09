5fb98f66ff96f879e18f1caac812a2c6a3d85262
CheloUY <mlopezlage@gmail.com>
2020-03-09 11:24:50 -0300

    estabilización en la creación de mascota

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -20,7 +20,7 @@ def mascota_view(request):
         form = MascotaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('mascota:mascota_listar')
+            return redirect(mascota_list)
     else:
         form = MascotaForm()
     return render(request, 'mascota/mascota_form.html', {'form': form})
